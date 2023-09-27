dd10ed58d65265e51ba9c49c94ffe8691b6c8ee3
NachoM57 <ignaciomaiorino50@gmail.com>
2023-09-27 16:06:53 -0300

    navbar lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './Componentes/inicio/inicio.component';
+import { NavbarComponent } from './Componentes/navbar/navbar.component';
+import { BannerComponent } from './Componentes/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NavbarComponent,
+    BannerComponent,
   ],
   imports: [
     BrowserModule,
