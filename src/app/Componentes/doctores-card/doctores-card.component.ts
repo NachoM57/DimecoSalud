40f6b92553fc83ddbe79db7205b9cb467621fbeb
NachoM57 <ignaciomaiorino50@gmail.com>
2023-11-02 20:06:20 -0300

    sacando efectosd aos y codigo ts

--- src/app/Componentes/doctores-card/doctores-card.component.ts
+++ src/app/Componentes/doctores-card/doctores-card.component.ts
@@ -8,7 +8,6 @@ import { DataService } from 'src/app/Componentes/service/data.service';
 })
 export class DoctoresCardComponent implements OnInit {
   doctores: any = []
-  showScrollToTopButton = false;
 
   constructor(private dataService: DataService) {
   }
@@ -20,28 +19,7 @@ export class DoctoresCardComponent implements OnInit {
       this.doctores = data.doctores;
       this.doctores = data.doctores;
     })
-
-    window.addEventListener('scroll', this.checkScroll);
   }
-
-
-  ngOnDestroy() {
-    // Es importante remover el listener cuando el componente ya no está en uso.
-    window.removeEventListener('scroll', this.checkScroll);
-  }
-
-  checkScroll = (): void => {
-    // Puedes ajustar el valor 100 al número de pixels que prefieras
-    this.showScrollToTopButton = window.pageYOffset > 100;
-  };
-
-  scrollToTop(): void {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  }
-
 
   transformToHtmlBreaks(text: string): string {
     return '<strong>' + text.replace(/\n\n/g, '<br>') + '</strong>';
