4b688b819476dcc9ff24daf3fb2d258e3cbe93db
NachoM57 <ignaciomaiorino50@gmail.com>
2024-01-25 19:26:09 -0300

    cambio de navbar

--- src/app/Componentes/navbar/navbar.component.ts
+++ src/app/Componentes/navbar/navbar.component.ts
@@ -24,12 +24,26 @@ export class NavbarComponent implements OnInit {
 
   }
 
+  menuValue: boolean = false;
+  menu_icon: string = 'fa-solid fa-bars';
+  openMenu() {
+    this.menuValue = !this.menuValue;
+    this.menu_icon = this.menuValue ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+  }
+  closeMenu() {
+    this.menuValue = false;
+    this.menu_icon = 'fa-solid fa-bars';
+  }
+
   navigateTo(event: Event, elementId: string): void {
     event.preventDefault(); // Prevenir la navegación por defecto
     const element = document.getElementById(elementId);
+
     if (element) {
       element.scrollIntoView({ behavior: 'smooth' });
+
+      // Cerrar el menú después de la navegación
+      this.closeMenu();
     }
   }
-
 }
